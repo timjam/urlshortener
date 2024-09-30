a2d431b0e681f5c84ffacd5d0865eef3243f2f41
Timo Mätäsaho <timo.matasaho@gmail.com>
2024-09-30 20:09:53 +0300

    Some existense checks to db driver

--- server/db/jsonDb.ts
+++ server/db/jsonDb.ts
@@ -1,9 +1,41 @@
 import { JsonDB, Config } from 'node-json-db';
 import * as crypto from 'crypto';
+import { z } from 'zod';
 
 const db = new JsonDB(new Config("shortUrls", true, true, '/'));
 
-export const addNewUrl = (longUrl: string) => {
+export const UrlStats = z.object({
+    shortUrl: z.string(),
+    originalUrl: z.string(),
+    accessed: z.record(z.number()),
+});
+
+export type UrlStats = z.infer<typeof UrlStats>;
+
+const urlExists = async (url: string) => {
+    try {
+        const urls = await db.getData('/');
+        const existing = urls.find((item: any) => item.originalUrl === url);
+
+        if (!existing) {
+            return null;
+        }
+        
+        return UrlStats.parse(existing);
+    } catch (error) {
+        console.error(`Faulty item in db - ${error}`);
+        return null;
+    }
+};
+
+export const addNewUrl = async (longUrl: string) => {
+
+    const exists = await urlExists(longUrl);
+
+    if (exists) {
+        return exists.shortUrl;
+    }
+
     const url = longUrl.split('://').pop();
 
     if (!url) {
@@ -12,11 +44,13 @@ export const addNewUrl = (longUrl: string) => {
 
     const shortUrl = crypto.createHash('md5').update(url).digest("hex")
 
-    db.push(`/${shortUrl}`, {
+    const urlStats: UrlStats = {
         shortUrl,
         originalUrl: longUrl,
         accessed: {}
-    });
+    };
+
+    db.push(`/${shortUrl}`, urlStats);
 
     return shortUrl;
 };
@@ -40,12 +74,19 @@ export const getOriginalUrl = async (shortUrl: string) => {
 
         return data.originalUrl;
     } catch (error) {
+        console.log('Error happened')
         console.error(error);
         return null;
     }
 };
 
 export const getStats = async (shortUrl: string) => {
-    const data = await db.getData(`/${shortUrl}`);
-    return data;
+    try {
+        const data = await db.getData(`/${shortUrl}`);
+        return data;
+
+    } catch (error) {
+        console.error(`Couldn't find stats for ${shortUrl} - ${error}`);
+        return null;
+    }
 }
